4c3f996e3a27a36e812fa15a5bf3b109596ab9a1
abdullahshams77 <abdullah.encode@gmail.com>
2024-04-17 19:02:17 +0500

    prime react theme configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import './theme.css'
+//import 'primereact/resources/themes/lara-light-green/theme.css';
+//import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en"> 
+      <body className={inter.className}>
+        <PrimeReactProvider>
+          {children}
+        </PrimeReactProvider>
+      </body>
     </html>
   );
 }
