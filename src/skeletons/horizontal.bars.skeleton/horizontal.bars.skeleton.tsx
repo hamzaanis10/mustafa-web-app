5276f3efe9c5e625b4223e0ad7f29c049e71b7f6
unknown <muhammad.shahiq@cloudasset.com>
2024-05-02 20:19:57 +0500

    style fix

--- src/skeletons/horizontal.bars.skeleton/horizontal.bars.skeleton.tsx
+++ src/skeletons/horizontal.bars.skeleton/horizontal.bars.skeleton.tsx
@@ -9,11 +9,11 @@ interface LoaderSkeletonProps {
 
 const HorizontalBarSkeleton: React.FC<LoaderSkeletonProps> = ({ count=7 }) => {
   const skeletonItems = Array.from({ length: count }).map((_, index) => (
-    <Skeleton key={index} width="20rem" height="3rem" className="mb-2" />
+    <Skeleton key={index} width="18rem" height="3rem" className="mb-2" />
   ));
   return (
     <div
-      className="flex flex-column min-h-screen"
+      className="flex flex-column min-h-screen p-3"
       style={{ background: "#fff" }}
     >
       {skeletonItems}
