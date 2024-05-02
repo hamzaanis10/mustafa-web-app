d3df339ec9fd46fda65e007706d19c2d0a8c091e
unknown <muhammad.shahiq@cloudasset.com>
2024-05-02 12:06:50 +0500

    home page banner

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -6,32 +6,37 @@ import MyCart from "../shopping.cart/shopping.cart";
 import Image from "next/image";
 import UserProfile from "../user.profile/user.profile";
 import CategoryMenuItems from "../menu.category/menu.catrgory";
+import { Sidebar } from "primereact/sidebar";
 
 const Header = () => {
-  const [isCategoryMenuVisible, setIsCategoryMenuVisible] = useState(false);
+  const [isCategoryMenuVisible, setIsCategoryMenuVisible] =
+    useState<boolean>(false);
 
-  const toggleCategoryMenu = () => {
-    setIsCategoryMenuVisible(!isCategoryMenuVisible);
-  };
+
+  const customHeader = <p className="font-bold">Categories</p>;
 
   return (
     <>
       {isCategoryMenuVisible && (
-        <div className="fixed z-5 bg-white h-screen">
-          <div className="flex justify-content-around items-center" style={{borderBottom:"1px solid #c4c4c4"}}>
-            <h3 className="text-lg font-semibold">Categories</h3>
-            <button
-              onClick={toggleCategoryMenu}
-              className="bg-white border-none"
-            >
-              <i className="pi pi-times"></i>
-            </button>
-          </div>
-          <CategoryMenuItems />
+        <div className="fixed z-5 bg-white h-screen card flex justify-content-center">
+          <Sidebar
+            visible={isCategoryMenuVisible}
+            onHide={() => setIsCategoryMenuVisible(false)}
+            header={customHeader}
+          >
+            <CategoryMenuItems />
+          </Sidebar>
         </div>
       )}
-      <header className="z-4 sticky top-0 flex flex-wrap align-items-center justify-content-between p-3 lg:flex-nowrap xl:pl-6 xl:pr-6" style={{backgroundColor:"#fff"}}>
-        <div className="pr-3 lg:hidden" onClick={toggleCategoryMenu}>
+      
+      <header
+        className="z-4 sticky top-0 flex flex-wrap align-items-center justify-content-between p-3 lg:flex-nowrap xl:pl-6 xl:pr-6"
+        style={{ backgroundColor: "#fff" }}
+      >
+        <div
+          className="pr-3 lg:hidden"
+          onClick={() => setIsCategoryMenuVisible(true)}
+        >
           <i className="pi pi-bars"></i>
         </div>
         <div className="flex-initial lg:flex-1">
