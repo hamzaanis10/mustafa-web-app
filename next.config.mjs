c78a641195c8eadfc7a16a9efd55a15e31673f13
abdullahshams555 <abdullah.shams@cloudasset.com>
2024-05-03 12:58:42 +0500

    banners changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        styledComponents: true,
-      },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mustafa-dev.s3.eu-north-1.amazonaws.com',
+        //port: '',
+        //pathname: '/account123/**',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'amazonaws.com',
+//         //port: '',
+//         //pathname: '/account123/**',
+//       },
+//     ],
+//   },
+// }
