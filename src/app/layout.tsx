ce3413db58a3e2dcb488851475452fdb03ca489f
unknown <muhammad.shahiq@cloudasset.com>
2024-05-13 18:29:37 +0500

    fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="../../../assets/images/must-fav-con-32-32.png" />
+      <link rel="icon" href="../../../assets/images/must-fav-icon-32-32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="../../../assets/images/must-fav-icon-180-180.png" />
       <body className={inter.className}>
         <RefToastProvider>
           <SWRProvider>
