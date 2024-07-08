f8712cadd3d8b2060cced7cc9d3e0c4d859d864a
programmer-4-life <201617abdullah@gmail.com>
2024-07-08 11:12:13 +0500

    breadcrumb added

---
+++ src/components/common/app.breadcrumb/app.breadcrumb.tsx
@@ -0,0 +1,13 @@
+import { BreadCrumb, BreadCrumbProps} from 'primereact/breadcrumb';
+import './app.breadcrumb.css';
+
+
+const AppBreadCrumb:React.FC<BreadCrumbProps>=(props:any) =>  {
+    const {model} = props
+     
+    return (
+        <BreadCrumb model={model} />
+    )
+}
+
+export default AppBreadCrumb;  
